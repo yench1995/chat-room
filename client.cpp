7e9861c2604d172ea93eb6783e44535d02206470
yench1995 <yench1995s@gmail.com>
2017-07-02 17:02:23 +0800

    the client part

--- client.cpp
+++ client.cpp
@@ -19,8 +19,83 @@
 }while(0)
 
 using namespace std;
+bool send_is_over = false;
+
+void *recv(void *arg)
+{
+    int sockfd = *(int *)arg;
+    int n;
+    char recv_line[MAX_LINE_LEN];
+    vector<char *> list;
+    while (true)
+    {
+        if (send_is_over)
+            break;
+        if ((n = recv(sockfd,recv_line, MAX_LINE_LEN, 0)) < 0)
+        {
+            fprintf(stderr, "recieve from server failed!\n");
+            break;
+        }
+        recv_line[n] = 0;
+        split(list, recv_line, 30);
+        for (int i = 0; i < list.size(); ++i)
+        {
+            if (strlen(list[i]) > 0)
+            {
+                printf(">>%s\n", list[i]);
+                fflush(stdout);
+            }
+        }
+    }
+}
 
 
+void *send(void *arg)
+{
+    int sockfd = *(int *)arg;
+    int n = 0;
+    char sendline[MAX_LINE_LEN];
+    while (true)
+    {
+        fgets(sendline, MAX_LINE_LEN, stdin);
+        sendline[strlen(sendline)-1] = 0;
+        if (strcmp(sendline, "quit") == 0)
+            break;
+
+        if (send(sockfd, sendline, strlen(sendline), 0))
+        {
+            printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
+            break;
+        }
+    }
+    send_is_over = true;
+}
+
+int initSock(char *ip, int port)
+{
+    struct sockaddr_in servaddr;
+    int sockfd;
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("create socket error: %s(errno: %d\n)\n", strerror(errno), errno);
+        return -1;
+    }
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip, &servaddr.sin_addr) <= 0)
+    {
+        printf("inet_pton error for %s\n", ip);
+        return -1;
+    }
+
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("connect error: %s(errno: %d)\n", strerror(errno), errno);
+        return -1;
+    }
+    return sockfd;
+}
 int main(int argc, char **argv)
 {
     int sockfd;
@@ -37,6 +112,7 @@ int main(int argc, char **argv)
     pthread_create(&thread_recv, NULL, recv, &sockfd);
     pthread_create(&thread_send, NULL, recv, &sockfd);
     pthread_join(thread_send, NULL);
-
-
+    pthread_join(thread_recv, NULL);
+    close(sockfd);
+    return 0;
 }
---
+++ server.h
@@ -0,0 +1,130 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <unordered_map>
+#include <set>
+#include <vector>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+using namespace std;
+
+#define MAX_THREAD_NUM 100
+#define MAX_LINE_LEN 4096
+
+#define __DEBUG__ 1
+#define DEBUG(argv, format...)do{\
+    if (__DEBUG__) {\
+        fprintf(stderr, argv, ##format);\
+    }\
+} while(0)
+
+class ChatServer;
+
+typedef void (ChatServer::*p_func)(char *arg, bool is_logged, int connfd, ChatServer *p_session,
+        string &usr_name);
+
+class ChatServer{
+    public:
+       unordered_map<string, p_func> m_func;
+       ChatServer(int p) : port(p), cur_connect_num(0), cur_user_num(0)
+    {
+        m_func["login"] = &ChatServer::login;
+        m_func["say"] = &ChatServer::say;
+        m_func["logout"] = &ChatServer::logout;
+        m_func["look"] = &ChatServer::look;
+        m_func["help"] = &ChatServer::help;
+    }
+
+       void login(char *arg, bool is_logged, int connfd, ChatServer *p_session,
+        string &usr_name);
+       void say(char *arg, bool is_logged, int connfd, ChatServer *p_session,
+        string &usr_name);
+       void logout(char *arg, bool is_logged, int connfd, ChatServer *p_session,
+        string &usr_name);
+       void look(char *arg, bool is_logged, int connfd, ChatServer *p_session,
+        string &usr_name);
+       void help(char *arg, bool is_logged, int connfd, ChatServer *p_session,
+        string &usr_name);
+       void broadcase(char *msg, int msg_len);
+       void analyse_cmd(char *buf, char *cmd, char *arg, bool is_logged);
+       int run();
+       int eventAccept();
+       int eventRecv(char *line, struct epoll_event &event);
+       int eventSend(char *line, struct epoll_event &event);
+       int hasUser(const string &name);
+       void removeUser(int connfd, bool is_logged);
+       void addUser(int connfd, const std::string &name);
+       int initSock();
+
+       inline bool isLogged(int connfd)
+       {
+           unordered_map<int, bool>::iterator iter = m_islogged.find(connfd);
+           return iter == m_islogged.end()? false:true;
+       }
+
+       inline void setLogged(int connfd, bool is_logged)
+       {
+           m_islogged[connfd] = is_logged;
+       }
+
+       inline void addConnfd(int connfd)
+       {
+           s_connfd.insert(connfd);
+           m_islogged[connfd] = false;
+           ++cur_connect_num;
+       }
+
+       inline void removeConnfd(int connfd)
+       {
+           s_connfd.erase(connfd);
+           m_islogged.erase(connfd);
+           --cur_connect_num;
+       }
+
+       inline int get_user_num()
+       {
+           return cur_user_num;
+       }
+
+       inline int get_connect_num()
+       {
+           return cur_connect_num;
+       }
+
+    private:
+       unordered_map<int, string> m_users;
+       set<string> s_users;
+       unordered_map<int, bool> m_islogged;
+
+       struct sockaddr_in servaddr, clientaddr;
+       int listenfd;
+       set<int> s_connfd;
+       int port;
+       int cur_connect_num;
+       int cur_user_num;
+       int epfd;
+       socklen_t client;
+       struct epoll_event ev, events[20];
+};
+
+typedef struct _thread_para_t
+{
+    ChatServer *p_session;
+    int connfd_index;
+}thread_para_t;
+
+int setnoblock(int);
+#endif
+
